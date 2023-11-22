ca987c66a9c7e8b86cc0225b58ab6b9275fc488a
hasanrazin <147445153+hasanrazin@users.noreply.github.com>
2023-11-22 22:19:25 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,18 +3,18 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = "1747534"
-    API_HASH = "5a2684512006853f2e48aca9652d83ea"
+    APP_ID = 6
+    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
     # the name to display in your alive message
-    ALIVE_NAME = "Toha Xd"
+    ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "postgres://aaihzmdv:hkmNX33AlD3p3deIiwLeUzTO3yYzkTS1@suleiman.db.elephantsql.com/aaihzmdv"
+    DB_URI = "Your value"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOHsBu3wEnkEtWJEqRI9ABGRhmUxOnJRLVobPMKvOze_msdp_2Tm4V8_1dDd2N-HSDVsLekGv0QY8uDOptYhjap-Tc9UwrjpcSWqsiiWCGH9Thr-qmBeA2N9VsbVNt6_18ssTQ_YhPUEBG4S6Z2Ist4Iq-f4pwrgETBaHlKfJ5hFJ0cLIvctccNDKycO8WPORjZsVMAveMiyU8pw38Yb_L8V7C0z6oSTE-Hh2KpEP8jceQACwSNpBgbrmttjNSl5hocc8h1l8fS3oh3Nj-jYKmDCjBo-bHu-n3h1lB0YrvFraSSjoVuir5RakZcGwOM9nLVeJ8e8wIdaoWNDvOo64rj7Ckm4="
+    STRING_SESSION = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "6525553088:AAEHMRy8bDrri55TBdGh3AdqHMqTlDwKHBA"
+    TG_BOT_TOKEN = "Your value"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = "-1004068098013"
+    PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     COMMAND_HAND_LER = "."
     # command hanler for sudo
